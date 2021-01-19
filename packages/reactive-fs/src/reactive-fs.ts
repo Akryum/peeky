6612681f8d3821304db01a8a9dc29eca8a11a028
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-01-19 20:06:57 +0100

    feat(reactive fs): file events handlers

--- packages/reactive-fs/src/reactive-fs.ts
+++ packages/reactive-fs/src/reactive-fs.ts
@@ -97,5 +97,24 @@ export async function createReactiveFileSystem (options: ReactiveFileSystemOptio
     list,
     watchList,
     destroy,
+    onFileAdd: (handler: (relativePath: string) => unknown) => {
+      watcher.on('add', (relativePath) => {
+        handler(relativePath)
+      })
+    },
+    onFileChange: (handler: (relativePath: string) => unknown) => {
+      watcher.on('change', (relativePath) => {
+        handler(relativePath)
+      })
+    },
+    onFileRemove: (handler: (relativePath: string) => unknown) => {
+      watcher.on('unlink', (relativePath) => {
+        handler(relativePath)
+      })
+    },
   }
 }
+
+type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T
+
+export type ReactiveFileSystem = Awaited<ReturnType<typeof createReactiveFileSystem>>
