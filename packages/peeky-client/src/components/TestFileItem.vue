f136ffcae89ff361deda3a1f00585eeec442dbed
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-01-20 17:08:06 +0100

    feat(client): style tweaks

--- packages/peeky-client/src/components/TestFileItem.vue
+++ packages/peeky-client/src/components/TestFileItem.vue
@@ -11,6 +11,7 @@ const props = defineProps({
   <div class="px-3 py-1 flex items-center hover:bg-gray-50 dark:hover:bg-gray-900 space-x-2">
     <StatusIcon
       :status="file.status"
+      class="w-4 h-4"
     />
     <span class="flex-1">
       {{ file.relativePath }}
