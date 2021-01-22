9b33663092fa34a608f25cb1751b48631cb85e26
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-01-22 18:02:48 +0100

    fix(server): error parsing console warning

--- packages/peeky-server/src/util.ts
+++ packages/peeky-server/src/util.ts
@@ -1,11 +1,22 @@
+import consola from 'consola'
+
 export function getSrcFile (path: string) {
   return path.replace('dist', 'src').replace('js', 'ts')
 }
 
 export function getErrorPosition (filePath: string, stack: string) {
-  const [result, line, col] = new RegExp(`${filePath}:(\\d+):(\\d+)`).exec(stack)
-  return {
-    line: parseInt(line),
-    col: parseInt(col),
+  const result = new RegExp(`${filePath}:(\\d+):(\\d+)`).exec(stack)
+  if (result) {
+    const [_, line, col] = result
+    return {
+      line: parseInt(line),
+      col: parseInt(col),
+    }
+  } else {
+    consola.warn(`Couldn't get position from error: ${stack}`)
+    return {
+      line: 1,
+      col: 1,
+    }
   }
 }
