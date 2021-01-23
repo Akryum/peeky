332ad82f01d626d7d998c5fda410fa0c670741fd
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-01-23 22:54:40 +0100

    test: fix???

--- packages/peeky-runner/src/build.spec.ts
+++ packages/peeky-runner/src/build.spec.ts
@@ -1,10 +1,10 @@
 /// <reference types="@peeky/runner"/>
 
-import { getCachePath } from './build'
-
 peeky.mockModule('esbuild', {})
 
-describe('build cache', () => {
+describe('build cache', async () => {
+  const { getCachePath } = await import('./build')
+
   it('generates a cache path', () => {
     sinon.stub(process, 'cwd').callsFake(() => '/home/acme/project')
     expect(getCachePath('/home/acme/project/src/test.spec.js')).to.equal('/home/acme/project/node_modules/.temp/peeky-build-cache/src_test_spec_js.json')
