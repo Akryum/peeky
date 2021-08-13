932b47e5558368ee80d703888a1222e023e08842
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-08-13 23:43:27 +0200

    docs: fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,8 @@
-module.exports = require('./packages/peeky-client/tailwind.config')
+const config = module.exports = require('./packages/peeky-client/tailwind.config')
+config.purge = [
+  './docs/**/*.{vue,js,ts,jsx,tsx,md}',
+  './docs/.vitepress/**/*.{vue,js,ts,jsx,tsx,md}',
+]
+config.corePlugins = {
+  preflight: false,
+}
