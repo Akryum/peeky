7db7670bb11334a30fbdcd6701635c21cb4dcad2
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-08-13 16:35:09 +0200

    chore: fix vite config

--- packages/peeky-client/vite.config.ts
+++ packages/peeky-client/vite.config.ts
@@ -4,8 +4,10 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  alias: {
-    tslib: 'tslib/tslib.es6.js',
+  resolve: {
+    alias: {
+      tslib: 'tslib/tslib.es6.js',
+    },
   },
   build: {
     outDir: resolve(__dirname, '../peeky-client-dist/dist'),
