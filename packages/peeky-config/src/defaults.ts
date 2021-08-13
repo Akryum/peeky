11e1f8b4721e025e688dff3721f4d9e1c4e2a123
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-08-13 10:38:15 +0200

    fix: ignore node_modules by default

--- packages/peeky-config/src/defaults.ts
+++ packages/peeky-config/src/defaults.ts
@@ -5,10 +5,10 @@ export const peekyConfigFileMatch = ['**/peeky.config.(js|ts)']
 export const defaultPeekyConfig: () => PeekyConfig = () => ({
   targetDirectory: process.cwd(),
   match: ['**/*.(spec|test).(ts|js)', '**/__tests__/**/*.(ts|js)'],
-  ignored: ['node_modules'],
+  ignored: ['**/node_modules/**'],
   watchMatch: ['**/*.(js|ts)'],
   watchBaseDirectory: null,
-  watchIgnored: ['node_modules'],
+  watchIgnored: ['**/node_modules/**'],
   watchThrottle: 100,
   emptySuiteError: false,
 })
