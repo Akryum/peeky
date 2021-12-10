b1746f61c5b923dbae3611485cfdcfc28cb33e6a
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-12-10 06:09:09 +0100

    types: moduleCache type

--- packages/peeky-runner/src/runtime/module-cache.ts
+++ packages/peeky-runner/src/runtime/module-cache.ts
@@ -1,4 +1,2 @@
-import type { ViteExecutionResult } from './vite.js'
-
-export const moduleCache: Map<string, Promise<ViteExecutionResult>> = new Map()
+export const moduleCache: Map<string, Promise<any>> = new Map()
 export const sourceMaps: Map<string, any> = new Map()
