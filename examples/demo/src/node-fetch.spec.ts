da17c8c07240ddd503f89a60d26d26f31e405ea0
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-12-19 18:54:23 +0100

    chore(demo): better node-fetch test

--- examples/demo/src/node-fetch.spec.ts
+++ examples/demo/src/node-fetch.spec.ts
@@ -1,5 +1,14 @@
+/* @peeky {
+  runtimeEnv: 'dom'
+} */
+
 import fetch from 'node-fetch'
 
-test('node-fetch', () => {
+test('node-fetch', async () => {
   expect(typeof fetch).toBe('function')
+
+  const result = await fetch('https://google.com')
+  const text = await result.text()
+  expect(typeof text).toBe('string')
+  expect(text.length).toBeGreaterThan(0)
 })
