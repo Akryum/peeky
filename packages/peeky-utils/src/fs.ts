591344be866579d52101f54de8e394b9f91ab344
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2021-12-26 17:53:11 +0100

    Bump Version

--- packages/peeky-utils/src/fs.ts
+++ packages/peeky-utils/src/fs.ts
@@ -1,6 +1,7 @@
 import { isAbsolute } from 'path'
 import { pathToFileURL } from 'url'
 
+// FIXME: We shoud probably use either `path.posix.normalize` or vite's `normalize`
 export function slash (path: string) {
   return path.replace(/\\/g, '/')
 }
