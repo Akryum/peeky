7aefaacd769ffddc677f833a2f5411089887e01b
Guillaume Chau <guillaume.b.chau@gmail.com>
2022-01-21 00:42:24 +0100

    feat(ui): improve structure to prevent flashing of test pane

--- packages/peeky-client/src/features/run/RunTestView.vue
+++ packages/peeky-client/src/features/run/RunTestView.vue
@@ -1,14 +1,29 @@
 <script lang="ts" setup>
+import BaseSplitPane from '../BaseSplitPane.vue'
 import TestFileAllView from '../test-file/TestFileAllView.vue'
 import TestFileOneView from '../test-file/TestFileOneView.vue'
 </script>
 
 <template>
-  <TestFileOneView
-    v-if="$route.query.fileSlug"
-  />
+  <BaseSplitPane
+    :default-split="25"
+    :min="5"
+    :max="70"
+    save-id="peeky-run-test-file-view"
+    class="h-full"
+  >
+    <template #first>
+      <TestFileOneView
+        v-if="$route.query.fileSlug"
+      />
 
-  <TestFileAllView
-    v-else
-  />
+      <TestFileAllView
+        v-else
+      />
+    </template>
+
+    <template #last>
+      <router-view />
+    </template>
+  </BaseSplitPane>
 </template>
--- packages/peeky-client/src/features/suite/SuitesView.vue
+++ packages/peeky-client/src/features/suite/SuitesView.vue
@@ -5,11 +5,9 @@ const filterFailed = ref(false)
 </script>
 
 <script lang="ts" setup>
-import BaseSplitPane from '../BaseSplitPane.vue'
 import BaseInput from '../BaseInput.vue'
 import SuiteItem from './SuiteItem.vue'
 import { SearchIcon } from '@zhuowenli/vue-feather-icons'
-import { compareStatus } from '../../util/status'
 import { NexusGenFieldTypes } from '@peeky/server/types'
 
 type TestSuite = Omit<NexusGenFieldTypes['TestSuite'], 'children'> & {
@@ -76,68 +74,54 @@ const tree = computed(() => {
 </script>
 
 <template>
-  <BaseSplitPane
-    :default-split="25"
-    :min="5"
-    :max="70"
-    save-id="peeky-run-test-file-view"
-    class="h-full"
-  >
-    <template #first>
-      <div class="h-full flex flex-col divide-y divide-gray-100 dark:divide-gray-800">
-        <slot name="toolbar" />
+  <div class="h-full flex flex-col divide-y divide-gray-100 dark:divide-gray-800">
+    <slot name="toolbar" />
 
-        <div class="flex-none h-10">
-          <BaseInput
-            v-model="searchText"
-            size="md"
-            placeholder="Filter tests..."
-            class="h-full"
+    <div class="flex-none h-10">
+      <BaseInput
+        v-model="searchText"
+        size="md"
+        placeholder="Filter tests..."
+        class="h-full"
+      >
+        <template #after>
+          <button
+            v-tooltip="'Click to filter on failed tests'"
+            class="text-xs px-2 rounded cursor-pointer leading-tight"
+            :class="[
+              filterFailed ? 'text-red-200 bg-red-600 font-bold' : 'text-red-600 dark:text-red-400 bg-red-100 dark:bg-red-900',
+            ]"
+            @click="filterFailed = !filterFailed"
           >
-            <template #after>
-              <button
-                v-tooltip="'Click to filter on failed tests'"
-                class="text-xs px-2 rounded cursor-pointer leading-tight"
-                :class="[
-                  filterFailed ? 'text-red-200 bg-red-600 font-bold' : 'text-red-600 dark:text-red-400 bg-red-100 dark:bg-red-900',
-                ]"
-                @click="filterFailed = !filterFailed"
-              >
-                {{ failedTestCount }}
-              </button>
+            {{ failedTestCount }}
+          </button>
 
-              <SearchIcon class="mx-3 text-gray-500" />
-            </template>
-          </BaseInput>
-        </div>
+          <SearchIcon class="mx-3 text-gray-500" />
+        </template>
+      </BaseInput>
+    </div>
 
-        <div class="flex-1 overflow-y-auto">
-          <SuiteItem
-            v-for="suite of tree"
-            :key="suite.id"
-            :suite="suite"
-            :run="run"
-            :search="{
-              searchReg,
-              filterFailed,
-            }"
-            :depth="-1"
-          />
+    <div class="flex-1 overflow-y-auto">
+      <SuiteItem
+        v-for="suite of tree"
+        :key="suite.id"
+        :suite="suite"
+        :run="run"
+        :search="{
+          searchReg,
+          filterFailed,
+        }"
+        :depth="-1"
+      />
 
-          <div
-            v-if="!suites.length"
-            class="my-12 flex items-center justify-center"
-          >
-            <div class="bg-gray-50 text-gray-600 dark:bg-gray-900 dark:text-gray-400 text-center px-4 py-3 rounded">
-              😿️ No test suites found
-            </div>
-          </div>
+      <div
+        v-if="!suites.length"
+        class="my-12 flex items-center justify-center"
+      >
+        <div class="bg-gray-50 text-gray-600 dark:bg-gray-900 dark:text-gray-400 text-center px-4 py-3 rounded">
+          😿️ No test suites found
         </div>
       </div>
-    </template>
-
-    <template #last>
-      <router-view />
-    </template>
-  </BaseSplitPane>
+    </div>
+  </div>
 </template>
